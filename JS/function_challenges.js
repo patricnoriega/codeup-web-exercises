273b67615f47de5f668e1564626186e91a161a00
patric noriega <patricnoriega@gmail.com>
2021-08-13 15:34:19 -0500

    extra work

---
+++ JS/function_challenges.js
@@ -0,0 +1,40 @@
+//1.
+function returnTwo() {
+    return 2;
+}
+console.log(returnTwo())
+
+//2.
+function sayHowdy() {
+    console.log("Howdy!");
+}
+
+//3.
+function returnName(name) {
+    return name;
+
+}
+console.log(returnName("Patric"));
+
+//4.
+function addThree(number) {
+    var result = number + 3;
+    return result;
+}
+console.log(addThree(5))
+
+//5.
+function sayString(string) {
+    return string;
+
+}
+console.log(sayString('codeup'));
+
+//6.
+function identity(input) {
+    return output;
+}
+
+function getRandomNumber(min, max) {
+    return number;
+}
