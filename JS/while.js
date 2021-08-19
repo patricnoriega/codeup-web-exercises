db7fbe35172c6ab71bf27012a53692d6b5b7397d
patric noriega <patricnoriega@gmail.com>
2021-08-18 21:31:53 -0500

    Loops exercises update

--- JS/while.js
+++ JS/while.js
@@ -1,22 +1,16 @@
-console.log("Just nod if you can hear me")
-// var i = 10;
-//
-// while (i < 10) {
-//     console.log('while loop iteration #' + i);
-//     i++;
-// }
-
+console.log("Just nod if you can hear me");
+// #1
 // var i = 2;
 // while (i <= 65536) {
 //     console.log(i);
 //     i = i * 2;
 // }
-
-var allCones = Math.floor(Math.random() * 50) + 50;
-do {
-   allCones = confirm("I have to sell: " + allCones + " cones before I can go home.")
-    var conesSold = Math.floor(Math.random() * 5) + 1;
-   conesSold = confirm("I sold: " + conesSold + " cone(s) to each customer so far today.")
-}while (conesSold < allCones);
-
-console.log(allCones /= conesSold)
+//
+// #2
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// do {
+//    allCones = confirm("I have to sell: " + allCones + " cones before I can go home.")
+//     var conesSold = Math.floor(Math.random() * 5) + 1;
+//
+//    conesSold = confirm("I sold: " + conesSold + " cone(s) to each customer so far today.")
+// }while (conesSold < allCones);
