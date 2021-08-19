c552bfcf78332dbdee39db996a3bccd1591e640c
patric noriega <patricnoriega@gmail.com>
2021-08-19 17:09:11 -0500

    feat: iterating update

---
+++ JS/iterating.js
@@ -0,0 +1,59 @@
+(function(){
+    "use strict";
+console.log("I'm batman")
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+            var names = ["patric" , "ethan", "evan", "rocco"];
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+            console.log(names.length);
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+        console.log(names[0]);
+        console.log(names[1]);
+        console.log(names[2]);
+        console.log(names[3]);
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+    var fruits = ["apple", "banana", "orange", "plums", "cherries"]
+    console.log(fruits[0]);
+
+    var clothes = ["shirt", "pants", "jeans", "sweaters", "dress"]
+    console.log(clothes[1]);
+
+    var numbers = ["1","2","3","4","5"]
+    console.log(numbers[4]);
+
+})();
