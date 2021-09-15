1da7c07845f5204fe2e178d8b25b205ee59927b8
patric noriega <patricnoriega@gmail.com>
2021-09-15 09:03:56 -0500

    feat: update

--- js/warmup.js
+++ js/warmup.js
@@ -26,3 +26,39 @@ function calculateDogAge(puppyAge, conversionNum) {
     return "my dog is " + humanYears + "  years old in dog years " + " and is " + puppyAge + " in dog years" ;
 }
 calculateDogAge();
+
+// Given arrays of strings that are orders of food in string form to your function, write a function named tacoTuesday that returns TRUE or FALSE if the array provided has any element mention the word ‘taco’ in its value.
+
+    var hasTacos = ['blueberry muffin', 'banana', 'yogurt', 'cereal', 'brisket and egg taco']
+    var noTacos = ['parfait', 'omlette de frumage', 'protein shake', 'fasting'];
+
+    tacoTuesday(hasTacos) //true
+    tacoTuesday(noTacos) //false
+
+    //my code
+    // function tacoTuesday(hasTacos) {
+    //     return hasTacos === "taco" || noTacos == "taco";
+    // }
+    //     function tacoTuesday(hasTacos, noTacos) {
+    //     if (hasTacos.includes("taco")){
+    //         return true;
+    //     }else{
+    //         if (noTacos.includes("taco")){
+    //             return false;
+    //             }
+    //         }
+    //     }
+
+    //code review
+    // function tacoTuesday(arr) {
+    //     for (var i = 0; i < arr.length; i++) {
+    //
+    //     }
+    //     if (arr[i].includes("taco")) {
+    //         return true;
+    //     }
+    //         return false;
+    // }
+
+
+
