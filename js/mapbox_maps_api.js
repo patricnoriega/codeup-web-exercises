51ac050295604f6431c5ffbdd442b472c139dc44
patric noriega <patricnoriega@gmail.com>
2021-10-05 10:24:52 -0500

    feat: 10:25 update

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -17,7 +17,6 @@ geocode("17619 La Cantera Pkwy #208, San Antonio, TX 78257",mapBoxKey).then(func
     new mapboxgl.Marker(
         {
         color: 'yellow',
-        draggable:true
     }
     )
         .setLngLat(results)
@@ -37,3 +36,41 @@ geocode("Våghustorget 1, 702 10 Örebro, Sweden", mapBoxKey).then(function (res
     map.flyTo({center:results})
 
 })
+
+geocode("Carrer del Correu Vell, 3, 08002 Barcelona, Spain", mapBoxKey).then(function (results) {
+    var popUp2 = new mapboxgl.Popup()
+        .setHTML("<em>La Maison</em>")
+
+    new mapboxgl.Marker(
+        {
+            color: 'red'
+        }
+    )
+        .setLngLat(results)
+        .setPopup(popUp2)
+        .addTo(map)
+
+    map.flyTo({center: results})
+})
+
+var restaurants = [
+    {
+        name: "Bakudan Ramen",
+        location: "The Rim, San Antonio TX"
+
+    },
+    {
+        name:"Dine",
+        location:"Orebro, Sweden"
+    },
+    {
+        name: "La Maison",
+        location:"Gothic Quarter, Barcelona Spain"
+    },
+]
+restaurants.forEach(function (restaurant,index) {
+    console.log("restaurant # " + (index + 1));
+    console.log("name: " + restaurant.name);
+    console.log("location: " + restaurant.location);
+    console.log("-------");
+});
